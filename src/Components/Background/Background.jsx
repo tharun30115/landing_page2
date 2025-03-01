ea2449212438d3154c0888b18f551158beb8ab29
Tharun <tharungowda30115@gmail.com>
2025-03-01 20:19:33 +0530

    updates

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -7,16 +7,16 @@ import image3 from "../../assets/image3.png";
 const Background = ({ playStatus, heroCount }) => {
   if (playStatus) {
     return (
-      <video className="background" autoPlay muted loop>
+      <video className="background fade-in" autoPlay muted loop>
         <source src={video1} type="video/mp4" />
       </video>
     );
   } else if (heroCount === 0) {
-    return <img src={image1} className="background" alt="" />;
+    return <img src={image1} className="background fade-in" alt="" />;
   } else if (heroCount === 1) {
-    return <img src={image2} className="background" alt="" />;
+    return <img src={image2} className="background fade-in" alt="" />;
   } else if (heroCount === 2) {
-    return <img src={image3} className="background" alt="" />;
+    return <img src={image3} className="background fade-in" alt="" />;
   }
 };
 
