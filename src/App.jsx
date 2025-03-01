0353cadb8c4a02c5ab65c698866b25d70c83ac7a
Tharun <tharungowda30115@gmail.com>
2025-03-01 12:30:32 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
 
 
 const App = () => {
 
-  let heroData = []
+  let heroData = [
+    {text1:"Drive into", text2:"what you Love"},
+    {text1:"Indulge", text2:"your Passions"},
+    {text1:"Give in to", text2:"your Passions"}
+  ]
+
+  const [heroCount,setHeroCount] = useState(2)
 
   return (
     <div>
