67e1a0dba0757a4a32a4c6f749671d70cc5be32e
Tharun <tharungowda30115@gmail.com>
2025-03-01 12:51:18 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import Background from "./Components/Background/Background"
 
 const App = () => {
 
@@ -10,10 +10,10 @@ const App = () => {
   ]
 
   const [heroCount,setHeroCount] = useState(2)
-
+  const [playStatus,setPlayStatus] = useState(false)
   return (
     <div>
-      hello
+      <Background/>
     </div>
   )
 }
