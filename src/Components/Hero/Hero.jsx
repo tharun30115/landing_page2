b9dc4fe809f3a85e7bc71e6ba214eb09a8dc9297
Tharun <tharungowda30115@gmail.com>
2025-03-01 19:20:22 +0530

    updates

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -21,6 +21,10 @@ const Hero = ({heroData, setHeroCount, heroCount,setPlayStatus, playStatus}) => 
           <li onClick={() => setHeroCount(1)} className={heroCount===1?"hero-dot orange" : "hero-dot"}></li>
           <li onClick={() => setHeroCount(2)} className={heroCount===2?"hero-dot orange" : "hero-dot"}></li>
         </ul>
+        <div className="hero-play">
+          <img onClick={()=> setPlayStatus(!playStatus)} src={playStatus?pause_icon:play_icon} alt="" />
+          <p>See the video</p>
+        </div>
       </div>
     </div>
   )
