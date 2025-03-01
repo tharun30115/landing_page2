0266c119e0b9a743a65eaf2a738469ac9f57ff0b
Tharun <tharungowda30115@gmail.com>
2025-03-01 18:24:58 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react"
-import Background from "./Components/Background/Background"
+import { useState } from "react";
+import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
-
   let heroData = [
-    {text1:"Drive into", text2:"what you Love"},
-    {text1:"Indulge", text2:"your Passions"},
-    {text1:"Give in to", text2:"your Passions"}
-  ]
+    { text1: "Drive into", text2: "what you Love" },
+    { text1: "Indulge", text2: "your Passions" },
+    { text1: "Give in to", text2: "your Passions" },
+  ];
 
-  const [heroCount,setHeroCount] = useState(2)
-  const [playStatus,setPlayStatus] = useState(false)
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
   return (
     <div>
-      <Background/>
+      <Background playStatus={playStatus} heroCount={heroCount} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
